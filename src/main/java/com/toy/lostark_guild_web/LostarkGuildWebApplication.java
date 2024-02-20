d4d6428bb8dd593131b5d9258a218e733f187641
Leegeonmin <kmlee97s@gmail.com>
2024-02-20 17:00:12 +0900

    git init

---
+++ src/main/java/com/toy/lostark_guild_web/LostarkGuildWebApplication.java
@@ -0,0 +1,13 @@
+package com.toy.lostark_guild_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkGuildWebApplication {
+//
+	public static void main(String[] args) {
+		SpringApplication.run(LostarkGuildWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/lostark_guild_web/LostarkGuildWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.lostark_guild_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkGuildWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
